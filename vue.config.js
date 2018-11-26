39d535c3e88c836393caeaf34f87273eb715e0ba
shuliuzhenhua <shuliuzhenhua.gmail.com>
2018-11-26 12:28:28 +0800

    添加 stylus 自动引入 和 reset.css

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
 module.exports = {
   pages: {
     index: {
@@ -22,5 +26,22 @@ module.exports = {
   },
   pluginOptions: {
     vconsole: { enable: true }
+  },
+  chainWebpack: config => {
+    config.resolve.alias.set('@$', resolve('src')).set('components', resolve('src/components'))
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
   }
+}
+/**
+ * styl 文件自动导入
+ * @param rule
+ */
+function addStyleResource (rule) {
+  rule
+    .use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [path.resolve(__dirname, './src/assets/styles/imports.styl')]
+    })
 }
